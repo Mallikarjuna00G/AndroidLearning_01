d31f94e9f34fa5898359251221e1dd78c6ac7626
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-07-05 20:27:11 +0530

    Project002: HappyBirthday: Background image to the app

--- HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ HappyBirthday/app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -16,6 +18,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,16 +34,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             HappyBirthdayTheme {
                 Scaffold(
-                    modifier = Modifier.fillMaxSize()
+                    modifier = Modifier
+                        .fillMaxSize()
                         .border(width = 1.dp, color = Color.Blue),
                     contentColor = MaterialTheme.colorScheme.background,
                     containerColor = Color.LightGray
                 ) { innerPadding ->
                     // A surface container using the 'background' color from the theme
-                    GreetingText(
+                    GreetingImage(
                         modifier = Modifier.padding(innerPadding),
-                        message = "Happy Birthday Raghva!!",
-                        from = "From Mallikarjuna"
+                        message = stringResource(R.string.happy_birthday_raghva),
+                        from = stringResource(R.string.signature_text)
                     )
                 }
             }
@@ -69,7 +75,28 @@ fun GreetingText(
             fontSize = 36.sp,
             modifier = Modifier
                 .padding(16.dp)
-                .align(alignment = Alignment.End)
+                .align(alignment = Alignment.CenterHorizontally)
+        )
+    }
+}
+
+@Composable
+fun GreetingImage(
+    message: String,
+    from: String,
+    modifier: Modifier = Modifier
+) {
+    val image = painterResource(id = R.drawable.birthdaybutterfly)
+    Box(modifier = modifier) {
+        Image(
+            painter = image,
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            alpha = 0.5F
+        )
+        GreetingText(
+            message = "Happy Birthday Raghva!!",
+            from = "From Mallikarjuna"
         )
     }
 }
@@ -78,6 +105,7 @@ fun GreetingText(
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayTheme {
-        GreetingText(message = "Happy Birthday Raghva!!", from = "From Mallikarjuna")
+//        GreetingText(message = "Happy Birthday Raghva!!", from = "From Mallikarjuna")
+        GreetingImage(message = "Happy Birthday Raghva!!", from = "From Mallikarjuna")
     }
 }
