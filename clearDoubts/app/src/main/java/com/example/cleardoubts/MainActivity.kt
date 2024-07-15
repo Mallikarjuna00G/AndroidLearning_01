93bb87c383ffc2b560275f15781d8e0d7a7e8407
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-07-15 13:47:38 +0530

    Project: clearDoubts: understand state in compose

--- clearDoubts/app/src/main/java/com/example/cleardoubts/MainActivity.kt
+++ clearDoubts/app/src/main/java/com/example/cleardoubts/MainActivity.kt
@@ -3,21 +3,26 @@ package com.example.cleardoubts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.cleardoubts.ui.theme.ClearDoubtsTheme
 
@@ -60,15 +65,158 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     Surface(
         modifier = modifier.fillMaxSize()
     ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Hello $name!",
+                modifier = modifier,
+            )
+            learningState(modifier = Modifier.fillMaxSize())
+        }
+    }
+}
+
+class LearningState(var var1: Int = 0, var var2: Int = 0) {}
+
+@Composable
+fun learningState(modifier: Modifier) {
+    Surface(
+        modifier = modifier
+            .fillMaxSize()
+    ) {
+        var buttonClickedState by remember { mutableStateOf(0) }
+        var learningState by remember { mutableStateOf(LearningState(var1 = 10, var2 = 10)) }
+//        var learningState = LearningState(var1 = 10, var2 = 10)
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            buttonsSection1(
+                buttonClickedStateInc = {
+                    buttonClickedState++
+                    learningState.var1++
+                    learningState.var2++
+                                        },
+                buttonClickedStateDec = {
+                    buttonClickedState--
+                    learningState.var1--
+                    learningState.var2--
+                                        },
+                modifier = modifier
+                    .weight(1f)
+            )
+            section2(
+                buttonClickedState = buttonClickedState,
+                learningState = learningState,
+                modifier = Modifier
+                    .weight(1f)
+            )
+            // Section3 also getting recomposed when buttonClickedState changes
+            section3(
+                learningState = learningState,
+                modifier = Modifier
+                    .weight(1f)
+            )
+        }
+    }
+}
+
+@Composable
+fun section3(learningState: LearningState, modifier: Modifier) {
+    Column(modifier = modifier) {
         Text(
-            text = "Hello $name!",
-            modifier = modifier,
+            text = "Section3: ",
+            modifier = Modifier
+        )
+        Text(
+            text = learningState.var1.toString(),
+            modifier = Modifier
+        )
+        Text(
+            text = learningState.var2.toString(),
+            modifier = Modifier
+                .weight(1f)
         )
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    theTheme()
+fun section2(buttonClickedState: Int, learningState: LearningState, modifier: Modifier) {
+    Column(modifier = modifier) {
+        Text(
+            text = "Section2: ",
+            modifier = Modifier
+        )
+//        Text(
+//            text = buttonClickedState.toString(),
+//            modifier = Modifier
+//        )
+        Text(
+            text = learningState.var1.toString(),
+            modifier = Modifier
+        )
+        Text(
+            text = learningState.var2.toString(),
+            modifier = Modifier
+                .weight(1f)
+        )
+    }
 }
+
+@Composable
+fun buttonsSection1(buttonClickedStateInc: () -> Unit, buttonClickedStateDec: () -> Unit, modifier: Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxWidth()
+            .border(1.dp, Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        buttonsSection2Concise(
+            buttonClickedStateIncDec = buttonClickedStateInc,
+            isNext = true,
+            modifier = Modifier
+                .weight(1f)
+        )
+        buttonsSection2Concise(
+            buttonClickedStateIncDec = buttonClickedStateDec,
+            isNext = false,
+            modifier = Modifier
+                .weight(1f)
+        )
+    }
+}
+
+@Composable
+fun buttonsSection2Concise(
+    buttonClickedStateIncDec: () -> Unit,
+    isNext: Boolean,
+    modifier: Modifier
+) {
+    var text = ""
+    if(isNext) {
+        text = "increment"
+    }
+    else {
+        text = "decrement"
+    }
+    Button(
+        onClick = buttonClickedStateIncDec,
+        modifier = modifier
+    ){
+        Text(text = "Increment")
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    theTheme()
+//}
